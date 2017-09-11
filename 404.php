471a434e3d0ecb011b5babaa32550124313d86ee
rspilhaus <rspilhaus@gmail.com>
2017-09-11 11:02:54 -0500

    404 button

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@
 	<div class="bigtext-right">
 		<p class="work-services">Couldn&rsquo;t find <?php echo $actual_link; ?></p>
 		<p>Well... this is embarrassing. Someone (but definitely not you) goofed and either provided you an incorrect link to my site or deleted the page that it was originally referring to. There are only about five pages on this site though, so it shouldn&rsquo;t be too hard to find what you&rsquo;re looking for.</p>
-		<a href="/" target="_blank" class="button">Go to Home Page</a>
+		<a href="/" class="button">Go to Home Page</a>
 	</div><!-- end .bigtext-right -->
 </section><!-- end .bigtext-wrap -->
 
