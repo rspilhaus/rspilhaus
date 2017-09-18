17f0d335b7fb7d9afcf3a60ea5eb9f8f029f68f4
rspilhaus <rspilhaus@gmail.com>
2017-09-18 11:26:18 -0500

    Semantics

--- index.php
+++ index.php
@@ -20,48 +20,48 @@
 	</div><!-- end .bigtext-right -->
 </section><!-- end .bigtext-wrap -->
 
-<section class="work">
-	<a href="/mbb-agency" class="work-image"><img src="/build/images/work/home/mbb.jpg" alt=""></a>
+<div class="work">
+	<a href="/mbb-agency" class="work-image"><img src="/build/images/work/home/mbb.jpg" alt="MBB Image"></a>
 	<div class="work-details">
 		<h2 class="work-title">MBB Agency Website</h2>
 		<p class="work-services">Front-end Development, WordPress Development, Wireframing</p>
 		<p class="work-summary">A Kansas City based agency, MBB has been combining great creative with strategic thinking since 1982 to deliver exceptional results for their clients. </p>
 		<a href="/mbb-agency" class="button">View Project</a>
 	</div><!-- end .work-details -->
-</section><!-- end .work -->
+</div><!-- end .work -->
 
-<section class="work">
-	<a href="/daily-rider" class="work-image"><img src="/build/images/work/home/thedailyrider.jpg" alt=""></a>
+<div class="work">
+	<a href="/daily-rider" class="work-image"><img src="/build/images/work/home/thedailyrider.jpg" alt="The Daily Rider Image"></a>
 	<div class="work-details">
 		<h2 class="work-title">The Daily Rider Website</h2>
 		<p class="work-services">Front-end Development, WordPress Development, Wireframing</p>
 		<p class="work-summary">The Daily Rider is a bike shop in Washington, DC that specializes in bikes for the everyday rider. 2014 Webby Award Honoree for the “Best Practices” category.</p>
 		<a href="/daily-rider" class="button">View Project</a>
 	</div><!-- end .work-details -->
-</section><!-- end .work -->
+</div><!-- end .work -->
 
-<section class="work">
-	<a href="/niagara" class="work-image"><img src="/build/images/work/home/niagara.jpg" alt=""></a>
+<div class="work">
+	<a href="/niagara" class="work-image"><img src="/build/images/work/home/niagara.jpg" alt="Niagara Image"></a>
 	<div class="work-details">
 		<h2 class="work-title">Niagara Starch Landing Page</h2>
 		<p class="work-services">Front-end Development, Wireframing</p>
 		<p class="work-summary">Niagara is a local Kansas City company with over a century of experience in helping people look their best and make a great impression.</p>
 		<a href="/niagara" class="button">View Project</a>
 	</div><!-- end .work-details -->
-</section><!-- end .work -->
+</div><!-- end .work -->
 
-<section class="work">
-	<a href="/fccpt" class="work-image"><img src="/build/images/work/home/fccpt.jpg" alt=""></a>
+<div class="work">
+	<a href="/fccpt" class="work-image"><img src="/build/images/work/home/fccpt.jpg" alt="FCCPT Image"></a>
 	<div class="work-details">
 		<h2 class="work-title">FCCPT Website</h2>
 		<p class="work-services">Front-end Development, Wireframing</p>
 		<p class="work-summary">FCCPT (Foreign Credentialing Commission on Physical Therapy) is an organization that assists foreign-educated physical therapists.</p>
 		<a href="/fccpt" class="button">View Project</a>
 	</div><!-- end .work-details -->
-</section><!-- end .work -->
+</div><!-- end .work -->
 
-<section class="work">
-	<a href="/emails" class="work-image"><img src="/build/images/work/home/email.jpg" alt=""></a>
+<div class="work">
+	<a href="/emails" class="work-image"><img src="/build/images/work/home/email.jpg" alt="Email Image"></a>
 	<div class="work-details">
 		<h2 class="work-title">HTML Emails</h2>
 		<p class="work-services">Responsive Design, Development, Template Creation</p>
@@ -70,7 +70,7 @@
 	</div><!-- end .work-details -->
 </section><!-- end .work -->
 
-<section class="bigtext-wrap is-offwhite" id="about">
+<div class="bigtext-wrap is-offwhite" id="about">
 	<div class="bigtext-left">
 		<h1 class="bigtext fit-text">It’s nice to<br>meet you<br>too.</h1>
 		<a href="mailto:mail@ryanspilhaus.com" class="button"><span>Get In Touch</span></a>
@@ -91,7 +91,7 @@
 			- HTML5 Banner Ads<br>
 		</p>
 	</div><!-- end .bigtext-right -->
-</section><!-- end .bigtext-wrap -->
+</div><!-- end .bigtext-wrap -->
 
 <?php 
 	include('footer.php'); 
